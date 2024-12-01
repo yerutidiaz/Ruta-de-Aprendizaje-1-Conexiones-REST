1729e2b97fde03686b73a9e1c15a35559aab7dc4
yerutidiaz <diazyeruti550@gmail.com>
2024-11-30 21:26:40 -0300

    Create Color.kt

---
+++ Color.kt
@@ -0,0 +1,11 @@
+ackage com.example.marsphotos.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// personalizados
+val LightPink = Color(0xFFF6C7D8)  
+val DarkGreen = Color(0xFF006400)
+val CancelButtonBorderGreen = Color(0xFF008000)  
+val White = Color(0xFFFFFFFF)  
+
+
