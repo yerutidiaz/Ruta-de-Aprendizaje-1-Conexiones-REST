25ed5de5218f2b3e60851264e311d16da9faaa61
yerutidiaz <diazyeruti550@gmail.com>
2024-11-30 21:22:31 -0300

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.racetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Surface
+import com.example.racetracker.ui.RaceTrackerApp
+import com.example.racetracker.ui.theme.RaceTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            RaceTrackerTheme {
+                Surface { RaceTrackerApp() }
+            }
+        }
+    }
+}
